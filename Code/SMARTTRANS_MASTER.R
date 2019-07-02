4d7f8a425b8e89b78d10608c67e63d0b6af4df62
Rob Marty <ramarty@email.wm.edu>
2019-07-02 11:12:59 -0400

    add master script

---
+++ Code/SMARTTRANS_MASTER.R
@@ -0,0 +1,32 @@
+# smarTTrans Master
+
+# Filepaths --------------------------------------------------------------------
+if(Sys.info()[["user"]] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/CrashMap-Nairobi"
+if(Sys.info()[["user"]] == "robmarty") project_file_path <- "~/Dropbox/World Bank/IEs/CrashMap-Nairobi"
+
+rawdata_file_path <- file.path(project_file_path, "Data", "RawData")
+finaldata_file_path <- file.path(project_file_path, "Data", "FinalData")
+
+# Packages ---------------------------------------------------------------------
+library(leaflet)
+library(dplyr)
+library(rgdal)
+library(rgeos)
+library(data.table)
+library(lubridate)
+library(ggplot2)
+library(raster)
+library(sp)
+library(spdep)
+library(broom)
+library(raster)
+library(parallel)
+library(pbmcapply)
+library(stringr)
+library(dplyr)
+library(readxl)
+library(haven)
+library(doBy)
+library(grid)
+library(gridExtra)
+library(gtable)
